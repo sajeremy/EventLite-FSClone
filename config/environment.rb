ab13088704c8ce7c5470c1352f596be754b1c087
“sajeremy” <9jsantiago4@gmail.com>
2022-12-03 12:33:32 -0500

    application and environment files update

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+#Format keys from backend to frontend (camelize)
+Jbuilder.key_format camelize: :lower
+Jbuilder.deep_format_keys true
