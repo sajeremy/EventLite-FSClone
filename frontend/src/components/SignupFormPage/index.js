42ab4d6277194cdc3317d8702376194ea5638219
“sajeremy” <9jsantiago4@gmail.com>
2022-12-04 20:39:51 -0500

    started initial sign up form page styling

--- frontend/src/components/SignupFormPage/index.js
+++ frontend/src/components/SignupFormPage/index.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Redirect } from "react-router-dom";
 import * as sessionActions from "../../store/session";
+import "./SignupForm.css";
 
 function SignupFormPage() {
   const dispatch = useDispatch();
@@ -37,91 +38,100 @@ function SignupFormPage() {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <>
       <div>
-        <div>
-          <label>Email address</label>
-        </div>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <div>
+              <label>Email address</label>
+            </div>
 
-        <input
-          type="text"
-          placeholder="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
+            <input
+              type="text"
+              placeholder="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+
+          <br></br>
+
+          <div>
+            <div>
+              <label>Confirm email</label>
+            </div>
+            <input
+              type="text"
+              placeholder="confirm email"
+              value={confirmEmail}
+              onChange={(e) => setConfirmEmail(e.target.value)}
+              required
+            />
+          </div>
+
+          <br></br>
+
+          <ul>
+            {errors.map((error) => (
+              <li key={error}>{error}</li>
+            ))}
+          </ul>
+
+          <div>
+            <div>
+              <label>First Name</label>
+            </div>
+            <input
+              type="text"
+              placeholder="First Name"
+              value={first_name}
+              onChange={(e) => setFirst_name(e.target.value)}
+              required
+            />
+          </div>
+
+          <br></br>
+
+          <div>
+            <div>
+              <label>Last Name</label>
+            </div>
+            <input
+              type="text"
+              placeholder="Last Name"
+              value={last_name}
+              onChange={(e) => setLast_name(e.target.value)}
+              required
+            />
+          </div>
+
+          <br></br>
+
+          <div>
+            <div>
+              <label>Password</label>
+            </div>
+            <input
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+
+          <br></br>
+
+          <button type="submit">Sign Up</button>
+        </form>
       </div>
-
-      <br></br>
-
-      <div>
-        <div>
-          <label>Confirm email</label>
-        </div>
-        <input
-          type="text"
-          placeholder="confirm email"
-          value={confirmEmail}
-          onChange={(e) => setConfirmEmail(e.target.value)}
-          required
-        />
-      </div>
-
-      <br></br>
-
-      <ul>
-        {errors.map((error) => (
-          <li key={error}>{error}</li>
-        ))}
-      </ul>
-
-      <div>
-        <div>
-          <label>First Name</label>
-        </div>
-        <input
-          type="text"
-          placeholder="First Name"
-          value={first_name}
-          onChange={(e) => setFirst_name(e.target.value)}
-          required
-        />
-      </div>
-
-      <br></br>
-
-      <div>
-        <div>
-          <label>Last Name</label>
-        </div>
-        <input
-          type="text"
-          placeholder="Last Name"
-          value={last_name}
-          onChange={(e) => setLast_name(e.target.value)}
-          required
-        />
-      </div>
-
-      <br></br>
-
-      <div>
-        <div>
-          <label>Password</label>
-        </div>
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-      </div>
-
-      <br></br>
-
-      <button type="submit">Sign Up</button>
-    </form>
+      <img
+        src="https://www.salserosenclave.com/wp-content/uploads/2017/08/fbevent_214045662456930.jpg"
+        alt=""
+      ></img>
+      <div></div>
+    </>
   );
 }
 
