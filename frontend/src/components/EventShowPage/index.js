680a1b2b0f48eefcf1223ef46f341fc50167eb37
“sajeremy” <9jsantiago4@gmail.com>
2022-12-11 16:15:46 -0500

    completed img header for index page

--- frontend/src/components/EventShowPage/index.js
+++ frontend/src/components/EventShowPage/index.js
@@ -2,6 +2,8 @@ import React, { useEffect } from "react";
 import { NavLink, useParams } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchEvent, getEvent } from "../../store/event";
+import "./EventShowPage.css";
+import { FaRulerCombined } from "react-icons/fa";
 
 const EventShowPage = () => {
   const { eventId } = useParams();
@@ -12,25 +14,41 @@ const EventShowPage = () => {
   }, [eventId, dispatch]);
 
   const event = useSelector(getEvent(eventId));
+
   if (!event) {
     return null;
   }
 
+  const renderBlurImg = async () => {
+    await event;
+    const blurredImage = document.getElementById("show-page-blurred-img");
+    blurredImage.style.backgroundImage = `url(${event.photoUrl})`;
+  };
+  renderBlurImg();
+
   return (
     <>
-      <img src={event.photoUrl} alt=""></img>
-      <div>
-        <strong>Title:</strong> {event.title}
+      <div className="show-page-img-banner-container">
+        <div id="border-show-page-blurred-img"></div>
+        <div id="hide-overflow">
+          <div id="show-page-blurred-img"></div>
+        </div>
+        <img className="img-test" src={event.photoUrl} alt=""></img>
       </div>
-      <div>
-        <strong>Body:</strong> {event.body}
-      </div>
-      <div>
-        <strong>Address:</strong> {event.address}
-      </div>
-      <div>
-        <strong>Category: </strong>
-        {event.category}
+      <div className="show-page-details-container">
+        <div className="show-page-title">
+          <h1>{event.title}</h1>
+        </div>
+        <div>
+          <strong>Body:</strong> {event.body}
+        </div>
+        <div>
+          <strong>Address:</strong> {event.address}
+        </div>
+        <div>
+          <strong>Category: </strong>
+          {event.category}
+        </div>
       </div>
     </>
   );
