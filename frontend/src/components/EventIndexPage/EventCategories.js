924b4c583eecde56db3be52bdbe6d09e18714a0f
“sajeremy” <9jsantiago4@gmail.com>
2023-01-02 11:17:44 -0500

    modified categories from links to buttons

--- frontend/src/components/EventIndexPage/EventCategories.js
+++ frontend/src/components/EventIndexPage/EventCategories.js
@@ -7,6 +7,31 @@ import { RiSuitcaseLine } from "react-icons/ri";
 import { GiMartini } from "react-icons/gi";
 import "./EventIndexPage.css";
 
+const handleMusic = () => {
+  window.scroll(0, 480);
+};
+const handleArt = () => {
+  window.scroll(0, 480);
+};
+const handleHoliday = () => {
+  window.scroll(0, 480);
+};
+const handleHealth = () => {
+  window.scroll(0, 480);
+};
+const handleHobby = () => {
+  window.scroll(0, 480);
+};
+const handleBusiness = () => {
+  window.scroll(0, 480);
+};
+const handleFood = () => {
+  window.scroll(0, 480);
+};
+const handleSport = () => {
+  window.scroll(0, 480);
+};
+
 const EventCategories = () => {
   return (
     <div className="home-category-tiles">
@@ -14,70 +39,70 @@ const EventCategories = () => {
         Check out trending categories
       </h3>
       <div className="tile-group">
-        <Link to="#">
+        <button onClick={handleMusic}>
           <div className="title">
             <div className="tile-icon">
               <CiMusicNote1 className="orange-icon" />
             </div>
             <div className="tile-name">Music</div>
           </div>
-        </Link>
-        <Link to="#">
+        </button>
+        <button onClick={handleArt}>
           <div className="title">
             <div className="tile-icon">
               <FaTheaterMasks className="orange-icon" />
             </div>
             <div className="tile-name">Performing & Visual Arts</div>
           </div>
-        </Link>
-        <Link to="#">
+        </button>
+        <button onClick={handleHoliday}>
           <div className="title">
             <div className="tile-icon">
               <BiImages className="orange-icon" />
             </div>
             <div className="tile-name">Holiday</div>
           </div>
-        </Link>
-        <Link to="#">
+        </button>
+        <button onClick={handleHealth}>
           <div className="title">
             <div className="tile-icon">
               <FaRegHeart className="orange-icon" />
             </div>
             <div className="tile-name">Health</div>
           </div>
-        </Link>
-        <Link to="#">
+        </button>
+        <button onClick={handleHobby}>
           <div className="title">
             <div className="tile-icon">
               <TbDeviceGamepad className="orange-icon" />
             </div>
             <div className="tile-name">Hobbies</div>
           </div>
-        </Link>
-        <Link to="#">
+        </button>
+        <button onClick={handleBusiness}>
           <div className="title">
             <div className="tile-icon">
               <RiSuitcaseLine className="orange-icon" />
             </div>
             <div className="tile-name">Business</div>
           </div>
-        </Link>
-        <Link to="#">
+        </button>
+        <button onClick={handleFood}>
           <div className="title">
             <div className="tile-icon">
               <GiMartini className="orange-icon" />
             </div>
             <div className="tile-name">Food & Drink</div>
           </div>
-        </Link>
-        <Link to="#">
+        </button>
+        <button onClick={handleSport}>
           <div className="title">
             <div className="tile-icon">
               <FaRunning className="orange-icon" />
             </div>
             <div className="tile-name">Sports & Fitness</div>
           </div>
-        </Link>
+        </button>
       </div>
     </div>
   );
